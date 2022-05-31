6b1fa9213f11ef2b5df931fe9ea58d1e1ab424e1
teunrompa <teunrompa@gmail.com>
2022-05-31 12:15:37 +0200

    Added movie's cpt #2

--- netvlies-movies/wp-content/themes/netvlies-movies/functions.php
+++ netvlies-movies/wp-content/themes/netvlies-movies/functions.php
@@ -1,6 +1,42 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+
+declare(strict_types=1);
+
+
+require_once(__DIR__ . '/inc/shortcodes.php');
+
+add_action('init', 'mv_init');
+
+function mv_init(){
+   add_theme_support( 'post-thumbnails' ); 
+   mv_setup_post_type();
+   mv_load_stylesheets();
+}
+
+function mv_load_stylesheets(){
+    wp_enqueue_style('style.css', get_stylesheet_directory_uri() . '/css/style.css');
+}
+
+function mv_setup_post_type(){
+    register_post_type('movies', array(
+        'labels' => array(
+            'name' => 'Movies',
+            'singular_name' => 'Movie'
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'movies'),
+        'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'page-attributes', 'post-formats'),
+        'taxonomies' => array('category', 'post_tag')
+    ));
+}
+
+function mv_get_movie_list(int $post_count) : WP_Query{
+    $args = array(
+        'post_type' => 'movies',
+        'posts_per_page' => -1
+    );
+    $movies = new WP_Query($args);
+    return $movies;
 }
 ?>
